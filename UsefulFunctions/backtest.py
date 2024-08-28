bef6f0cb05119dd507cb57c3fb10b7da79637cc0
Yaros <yarosinteriano@gmail.com>
2024-08-28 16:04:44 -0600

    Enhance plotting functions in "backtest.py" with Cufflinks library
    - Renamed "Plot_test" function to "plot_test_returns" for clarity. Enhanced functionality to dynamically plot cumulative returns of multiple backtests using Cufflinks.
    - Updated "compute_model_accuracy" function to visualize outcomes with interactive Cufflinks plots.
    - Improved "strategy_drawdown" function to plot drawdown graphs using the Cufflinks library for better visual representation.

--- UsefulFunctions/backtest.py
+++ UsefulFunctions/backtest.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from UsefulFunctions import data
 
 
+
 def compute_strategy_returns(y_test, y_pred):
     """
     Computes the strategy returns by comparing real percent changes with model predictions.
@@ -39,26 +40,54 @@ def compute_strategy_returns(y_test, y_pred):
 
 
 
-def plot_returns(returns_serie):
+def plot_test_returns(returns, name=" "):
     """
-    Plots the cumulative percentage returns from a trading strategy.
+    Plots the cumulative returns of one or multiple test sets along with a break-even line.
 
-    This function takes a series of trading strategy returns, computes the cumulative sum, and 
-    plots it as a percentage. The plot visualizes the profit and loss (P&L) over time.
+    This function takes a 2D array-like object `returns` and converts it into a pandas DataFrame, 
+    or takes an already pandas DataFrame with multiple columns of returns, one for each test,
+    plots the cumulative returns for each column as a separate line plot. It also includes 
+    a horizontal break-even line at y = 0 to indicate no profit or loss.
 
-    Args:
-        returns_serie (pandas.Series): A series containing the returns from the trading strategy.
+    Parameters:
+    -----------
+    returns : array-like
+        A 2D array-like object (such as a list of lists or a pandas DataFrame) where each column 
+        represents the returns of a different test set over time.
+
+    name : string, optional
+        A string to be used in the plot title to describe the dataset or the specific test set. 
+        Default is an empty string, which will result in the title "Cumulative Returns" without 
+        additional description.
 
     Returns:
-        None: The function generates and displays a plot.
+    --------
+    None
+        The function displays the plot using Plotly but does not return any value.
     """
-    
-    (np.cumsum(returns_serie)*100).plot(figsize=(15,6))
-    plt.xlabel('Year', fontsize=20)
-    plt.ylabel('P&L in %', fontsize=20)
-    plt.title('Cumulative Returns From Strategy', fontsize=20)
-    plt.show()
 
+    df = pd.DataFrame(returns)
+
+    # Create a subplot
+    fig = make_subplots(rows=1, cols=1)
+
+    # Add cumulative returns data for test set
+    for i in range(len(df.columns)):
+        trace_test = go.Scatter(x=df.index, y=df.iloc[:, i].cumsum() * 100, mode="lines", name=f"Retruns Test {i}")
+        fig.add_trace(trace_test)
+    
+    # Add break-even line
+    trace_break_even = go.Scatter(x=df.index, y=[0] * len(df.index), mode="lines", name="Break-even", line=dict(color="red"))
+    fig.add_trace(trace_break_even)
+
+    # Set layout
+    fig.update_layout(title=f"Cumulative Returns {name}", xaxis_title="Time", yaxis_title="P&L in %", height=450)
+
+    # Display the plot
+    pyo.iplot(fig)
+    print(f"Profits : {'%.2f' % (df.cumsum().iloc[-1].sum() * 100)}%")
+
+    
 
 def vectorize_backtest_returns(returns, anualization_factor, benchmark_asset=".US500Cash", mt5_timeframe=mt5.TIMEFRAME_H1):
     """
@@ -173,8 +202,15 @@ def compute_model_accuracy(real_positions, predicted_positions):
     - Model accuracy percentage.
     """
     
-    # Creating Dataframe with real positons and predicted positions
-    df_accuracy = pd.DataFrame(real_positions)
+    import pandas as pd
+    import numpy as np
+    import cufflinks as cf
+
+    # Initialize Cufflinks in offline mode
+    cf.go_offline()
+
+    # Creating Dataframe with real positions and predicted positions
+    df_accuracy = pd.DataFrame(real_positions, columns=["real_position"])
     df_accuracy["pred_position"] = predicted_positions
     
     # Assigning 1 if the position forecasted is equal to the real position and 0 otherwise
@@ -188,16 +224,22 @@ def compute_model_accuracy(real_positions, predicted_positions):
     print("Counts of 1 indicate instances where the predicted position matched the real position.\n")
     print(accuracy)
 
-    # Total counts of occurrences where model was right (number assigned 1) divided into the total number of predictions
+    # Total counts of occurrences where the model was right (number assigned 1) divided by the total number of predictions
     model_accuracy = accuracy[1] / len(df_accuracy)
     print(f"\nModel has an accuracy of: {model_accuracy * 100:.2f}%")
-
-    plt.hist(df_accuracy["accuracy"], bins=3)
-    plt.xticks([0, 1])
-    plt.title("Model Accuracy", fontsize=20)
-    plt.ylabel("Counts", fontsize=15)
-    plt.xlabel("Distribution", fontsize=15);
-
+    
+    # Plotting the accuracy of the model in a histogram using the dynamic plot with Cufflinks
+    df_accuracy["accuracy"].iplot(
+        kind="hist",       
+        xTitle="Distribution", 
+        yTitle="Counts",
+        title="Model Accuracy",
+        bargap=0.2,
+        theme="white",         
+        colors=["blue"],
+        #layout=dict(height=400)
+    )
+        
     return df_accuracy
 
 
@@ -233,11 +275,20 @@ def strategy_drawdown(return_series):
     # Computing the drawdown
     drawdown = cumulative_return / running_max - 1
 
-    plt.figure(figsize=(15, 4))
-    plt.fill_between(drawdown.index, drawdown * 100, 0, drawdown, color="red", alpha=0.70)
-    plt.title("Strategy Drawdown", fontsize=20)
-    plt.ylabel("Drawdown %", fontsize=15)
-    plt.show()
+    fig = make_subplots(rows=1, cols=1)
+
+    trace_test = go.Scatter(x=drawdown.index, 
+                            y=drawdown * 100, 
+                            mode="lines", 
+                            fill="tozeroy", 
+                            name="Drawdown", 
+                            line=dict(color="red"),
+                            fillcolor="rgba(255, 0, 0, 0.8)") # Red color with 0.8 opacity
+                            
+    fig.add_trace(trace_test)
+    
+    fig.update_layout(title="Strategy Drawdown", xaxis_title="Time", yaxis_title="Drawdown in %", height=450, showlegend=True)
+    pyo.iplot(fig)
 
     maximum_drawdown = np.min(drawdown) * 100
     print(f"Max Drawdown: {'%.2f' % maximum_drawdown}%")
